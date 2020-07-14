a983f7433999cbf7720b985f962d25b94634336a
Dohyun Lee <xxrcy32@gmail.com>
2020-07-14 21:02:15 +0900

    랜덤 룰렛 돌리기 구현

---
+++ WhatToEAT/app/src/main/java/com/example/whattoeat/BaseColor.kt
@@ -0,0 +1,21 @@
+package com.example.whattoeat
+
+import android.graphics.Color
+
+class BaseColor {
+    val CHART_COLOR = intArrayOf(
+        Color.rgb(141, 182, 199),
+        Color.rgb(193, 179, 142),
+        Color.rgb(202, 159, 146),
+        Color.rgb(249, 205, 151),
+        Color.rgb(177, 194, 122),
+        Color.rgb(178, 226, 137),
+        Color.rgb(81, 192, 191),
+        Color.rgb(99, 173, 208),
+        Color.rgb(112, 149, 225),
+        Color.rgb(150, 163, 227),
+        Color.rgb(201, 147, 212),
+        Color.rgb(209, 141, 178),
+        Color.rgb(241, 195, 208)
+    )
+}
--- WhatToEAT/app/src/main/java/com/example/whattoeat/RouletteActivity.kt
+++ WhatToEAT/app/src/main/java/com/example/whattoeat/RouletteActivity.kt
@@ -1,61 +1,82 @@
 package com.example.whattoeat
 
+import android.content.Intent
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.animation.Animation
-import android.view.animation.AnimationUtils
+import android.view.View
 import android.widget.Button
-import androidx.core.content.ContentProviderCompat.requireContext
-import androidx.core.content.ContextCompat
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import com.github.mikephil.charting.animation.Easing
 import com.github.mikephil.charting.charts.PieChart
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
-import com.github.mikephil.charting.utils.ColorTemplate
+import java.util.*
 
 class RouletteActivity : AppCompatActivity() {
 
-//    private lateinit var restList: List<String>
     private lateinit var pieDataSet: PieDataSet
     private lateinit var pieData: PieData
     private lateinit var pieChart: PieChart
 
     private lateinit var rotateButton: Button
-//    private lateinit var intentList: ArrayList<String>
+    private lateinit var nextButton: Button
+    private lateinit var againButton: Button
+
+    private lateinit var backButton: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_roulette)
 
-        rotateButton = findViewById(R.id.rotate_button)
-//        intentList = intent.getStringArrayListExtra("restaurant")
+        init()
         setPieChart()
 
         rotateButton.setOnClickListener {
             rotationRoulette()
+            settingButton()
+        }
+        againButton.setOnClickListener {
+            rotationRoulette()
+        }
+        nextButton.setOnClickListener {
+            val intent: Intent = Intent(this, RandomActivity::class.java)
+//            intent.putExtra()
+            startActivity(intent)
+        }
+        backButton.setOnClickListener{
+            finish()
         }
     }
 
-    private fun setPieChart(){
-        val entry = mutableListOf<PieEntry>()
-//        val cnt: Float = (100 / intentList.size).toFloat()
+    private fun init() {
+        rotateButton = findViewById(R.id.rotate_button)
+        againButton = findViewById(R.id.again_button)
+        nextButton = findViewById(R.id.next_button)
 
-//        for (item in intentList) {
-//            entry.add(PieEntry(cnt, item))
-//        }
+        backButton = findViewById(R.id.back_button)
+
+        againButton.visibility = View.GONE
+        nextButton.visibility = View.GONE
+    }
+
+    private fun setPieChart() {
+        val entry = mutableListOf<PieEntry>()
 
         entry.add(PieEntry(2f, "한식"))
         entry.add(PieEntry(2f, "양식"))
         entry.add(PieEntry(2f, "중식"))
         entry.add(PieEntry(2f, "일식"))
         entry.add(PieEntry(2f, "분식"))
+        entry.add(PieEntry(2f, "패스트푸드"))
+        entry.add(PieEntry(2f, "야식"))
+        entry.add(PieEntry(2f, "기타"))
 
         pieDataSet = PieDataSet(entry, "")
-        val chartColor = ColorTemplate.PASTEL_COLORS.toMutableList()
-//        chartColor.add(ContextCompat.getColor(requireContext(), R.color.colorPrimary))
+        val baseColor: BaseColor = BaseColor()
+        val chartColor = baseColor.CHART_COLOR.toMutableList()
 
         pieDataSet.apply {
             colors = chartColor
@@ -70,21 +91,27 @@ class RouletteActivity : AppCompatActivity() {
             holeRadius = 0f
             centerText = ""
             isDrawHoleEnabled = false
-            setEntryLabelColor(Color.WHITE)
+            setEntryLabelColor(Color.DKGRAY)
             setEntryLabelTextSize(16f)
         }
+        pieChart.legend.isEnabled = false
+        pieDataSet.setDrawValues(false)
         pieChart.invalidate()
     }
 
     private fun rotationRoulette() {
-        val animation: Animation = AnimationUtils.loadAnimation(applicationContext, R.anim.rotate)
-        var randomNum : Float = Math.random().toFloat() + 1000
-//        animation.repeatCount = randomNum
-
-//        pieChart.spin(1000, 0f, randomNum, Easing.EaseInOutQuad)
-        pieChart.spin(1000, 0f, 1000f, Easing.EasingFunction {
-            Easing.EaseInQuad
-            randomNum })
+        var random: Random = Random()
+        val randomNum = random.nextInt(360) + 1440
+        Log.d("!!!", randomNum.toString())
+        pieChart.spin(1000, -19f, -randomNum.toFloat(), Easing.EaseInOutQuad)
+//        pieChart.spin(500, -19f, -19f, Easing.EaseInOutQuad)
     }
+
+    private fun settingButton() {
+        againButton.visibility = View.VISIBLE
+        nextButton.visibility = View.VISIBLE
+        rotateButton.visibility = View.GONE
+    }
+
 
 }
