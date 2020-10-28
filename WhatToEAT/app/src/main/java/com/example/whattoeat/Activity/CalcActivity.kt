fb7798fec72d344a87935af50a1949a16a2a75e2
Dohyun Lee <xxrcy32@gmail.com>
2020-10-28 15:59:32 +0900

    화살표 rotate animation 기능 구현

--- WhatToEAT/app/src/main/java/com/example/whattoeat/Activity/CalcActivity.kt
+++ WhatToEAT/app/src/main/java/com/example/whattoeat/Activity/CalcActivity.kt
@@ -1,11 +1,14 @@
 package com.example.whattoeat.Activity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.whattoeat.R
+import java.util.*
 
 class CalcActivity : AppCompatActivity() {
 
@@ -18,8 +21,12 @@ class CalcActivity : AppCompatActivity() {
 
         Toast.makeText(this, "핸드폰을 기준으로 둥글게 앉으면 좋아요!", Toast.LENGTH_SHORT).show()
 
-//        rotateButton.setOnClickListener {
-//            arrow.animation =
-//        }
+        rotateButton.setOnClickListener {
+            val random = Random()
+            val randomNum = random.nextInt(360) + 1440
+            ObjectAnimator.ofFloat(arrow, View.ROTATION, -360f, (randomNum+100).toFloat()).apply {
+                duration = 3000
+            }.start()
+        }
     }
 }
