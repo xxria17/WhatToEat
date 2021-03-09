6aec1a63400274b061f025d820abc33314f40d06
xxria17 <xxrcy32@gmail.com>
2021-03-09 16:46:29 +0900

    룰렛 컬러 갯수 추가

---
+++ WhatToEAT/app/src/main/java/com/dohxxn/whattoeat/BaseColor.kt
@@ -0,0 +1,30 @@
+package com.dohxxn.whattoeat
+
+import android.graphics.Color
+
+class BaseColor {
+    val CHART_COLOR = intArrayOf(
+        Color.rgb(162,208,218), //1
+        Color.rgb(236,195,170),
+        Color.rgb(225,166,181),
+        Color.rgb(239,210,218),
+        Color.rgb(193,160,190), //5
+        Color.rgb(239,239,170),
+        Color.rgb(237,201,183),
+        Color.rgb(238,140,129),
+        Color.rgb(190,212,192),
+        Color.rgb(153,210,206), //10
+        Color.rgb(239,186,178),
+        Color.rgb(238,188,152),
+        Color.rgb(143,181,161),
+
+        Color.rgb(162,208,218),
+        Color.rgb(236,195,170), //15
+        Color.rgb(225,166,181),
+        Color.rgb(239,210,218),
+        Color.rgb(193,160,190),
+        Color.rgb(239,239,170),
+        Color.rgb(237,201,183) //20
+
+    )
+}
